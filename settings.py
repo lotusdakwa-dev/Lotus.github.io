82346d173d2c1ab826ac6ebd47c048109aa6e196
Dexy Dakwa <lotusdakwa@gmail.com>
2026-07-14 03:58:00 +0100

    Add files via upload

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--+hg!yn)764*j6ff=@araltn_^fkr8s^7y5p^33jkqa%vpbc@+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'lotus-github-io.onrender.com']
 
 
 # Application definition
